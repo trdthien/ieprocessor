f20b484074cb0896bf5439a9321d340cf7895f9e
Trinh Duc Thien <td.thien@shopmacher.de>
2017-11-29 09:39:20 +0700

    convert cent amount

---
+++ src/Shopmacher/IEProcessor/Csv/Helper/ConvertCentAmount.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Shopmacher\IEProcessor\Csv\Helper;
+
+/**
+ * Class ConvertCentAmount
+ * @package Shopmacher\IEProcessor\Csv\Helper
+ */
+class ConvertCentAmount implements ConverterInterface
+{
+    const PATTERN = '/^cent\((.*)\)$/';
+
+    /**
+     * @inheritdoc
+     */
+    public function matching(&$key)
+    {
+        if (preg_match(self::PATTERN, $key)) {
+            $key = preg_replace(self::PATTERN, '$1', $key);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param mixed $value
+     * @param array $raw
+     * @return int
+     */
+    public function apply($value, $raw)
+    {
+        $value = (float) $value;
+        return (int) $value * 100;
+    }
+}
